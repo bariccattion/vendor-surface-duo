c6e3b5d214a93440690fb95bd31d1f18335b1e94
Bariccatti <bariccatti@gmail.com>
2026-04-18 16:34:37 -0300

    Rename vendor path from duo-de to surface-duo

--- config/common.mk
+++ config/common.mk
@@ -1,25 +1,25 @@
 # DUO-DE Vendor Config — unified from ponces/vendor_ponces + Archfx/duoVendor + Archfx/duoOverlays
 
 # Overlay
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/duo-de/overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/duo-de/overlay/common
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/surface-duo/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/surface-duo/overlay/common
 
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/duo-de/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+    vendor/surface-duo/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
 
 # Power whitelist
 PRODUCT_COPY_FILES += \
-    vendor/duo-de/config/permissions/custom-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/custom-power-whitelist.xml
+    vendor/surface-duo/config/permissions/custom-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/custom-power-whitelist.xml
 
 # DUO-DE privapp permissions
 PRODUCT_COPY_FILES += \
-    vendor/duo-de/config/permissions/privapp-permissions-duo.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-duo.xml
+    vendor/surface-duo/config/permissions/privapp-permissions-duo.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-duo.xml
 
 # Boot animation
 PRODUCT_COPY_FILES += \
-    vendor/duo-de/prebuilts/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
-    vendor/duo-de/prebuilts/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
+    vendor/surface-duo/prebuilts/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
+    vendor/surface-duo/prebuilts/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
 
 # Enable lockscreen live wallpaper
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
